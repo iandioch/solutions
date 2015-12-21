c4b5241be6028fbf203cbdfb37cb0fda09e9f793
iandioch <iandioch11@gmail.com>
2015-12-21 18:37:43 +0000

    Add advent of code day 4 part 1

---
+++ advent_of_code_2015/04/part1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"crypto/md5"
+	//"encoding/hex"
+	"strconv"
+)
+
+func main() {
+
+	key := "yzbqklnj"
+	for i := 1; ; i ++ {
+		k := key + strconv.Itoa(i)
+
+		hexa := fmt.Sprintf("%x", md5.Sum([]byte(k)))
+
+		if hexa[0:5] == "00000" {
+			fmt.Println(i, hexa)
+			return
+		}
+	}
+}
