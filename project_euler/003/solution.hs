a6413a723c05cbf772ef5e965880f80d7a2682d0
iandioch <iandioch11@gmail.com>
2016-01-03 20:26:08 +0000

    Add project euler #3 in Haskell

---
+++ project_euler/003/solution.hs
@@ -0,0 +1,9 @@
+factors :: Int -> [Int]
+--factors n :: Integer -> Integer[]
+factors n = (filter
+	(\x -> n `mod` x == 0)
+	[2,3..((floor $ sqrt $ fromIntegral n)+1)])	
+
+isPrime n = (length (factors n)) == 0
+
+main = (print (filter isPrime (factors 600851475143)))
