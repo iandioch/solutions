32d6165d014a0aa67df07d7178bbe4f93f2b3ad8
iandioch <iandioch11@gmail.com>
2016-01-03 20:29:17 +0000

    Refactor project euler #3

--- project_euler/003/solution.hs
+++ project_euler/003/solution.hs
@@ -4,6 +4,6 @@ factors n = (filter
 	(\x -> n `mod` x == 0)
 	[2,3..((floor $ sqrt $ fromIntegral n)+1)])	
 
-isPrime n = (length (factors n)) == 0
+isPrime n = (length $ factors n) == 0
 
-main = (print (filter isPrime (factors 600851475143)))
+main = print $ last  $ filter isPrime (factors 600851475143)
