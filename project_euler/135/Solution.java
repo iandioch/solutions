a7f144694b858a5959c3e8c32c5cd5abb3e0139b
iandioch <iandioch11@gmail.com>
2016-04-11 12:31:40 +0100

    Add project euler prob 135 in Java

---
+++ project_euler/135/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args){
+        int[] n = new int[1000000];
+        for(int z = 1; z < n.length; z ++){
+            for(int d = z/3 + 1; d < n.length; d ++){
+                int m = 2*d*z + 3*d*d - z*z;
+                if(m < 0){
+                    //System.out.println(z + " " + d);
+                    break;
+                }
+                if (m >= n.length){
+                    continue;
+                }
+                n[m] += 1;
+            }
+        }
+        int ans = 0;
+        for(int i : n){
+            if(i == 10){
+                ans ++;
+            }
+        }
+        
+        System.out.println(ans);
+    }
+}
