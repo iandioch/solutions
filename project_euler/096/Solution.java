aa3fd034df6f49a053fd74c035088a466ea382dc
Noah O Donnaile <iandioch11@gmail.com>
2016-11-09 04:35:43 +0000

    Refactor and optimise project euler 096 Java

--- project_euler/096/Solution.java
+++ project_euler/096/Solution.java
@@ -1,40 +1,34 @@
-import java.io.*;
-import java.util.*;
+// Brute force, same logic as Python solution. Runs in < 4 seconds
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.BitSet;
 
 class Solution {
-
-	private static int[] copyArray(int[] in) {
-		int[] out = new int[in.length];
-		System.arraycopy(in, 0, out, 0, in.length);
-		return out;
-	}
-
 	private static int solve(int[] a) {
 		int i = 0;
-		while (i < a.length) {
-			if (a[i] == 0) break;
+		while (i < a.length && a[i] != 0) {
 			i ++;
 		}
 		if (i == a.length) return a[0]*100 + a[1]*10 + a[2];
 
-		int answer = -1;
-		HashSet<Integer> taken = new HashSet<>();
+		BitSet taken = new BitSet(10);
 		for (int p = 0; p < 81; p ++) {
 			if ((p/9 == i/9) || // same row
 			    ((p-i) % 9 == 0) || // same col
 			    ((p/27 == i/27) && ((p%9)/3 == (i%9)/3))) { // same square
-				taken.add(a[p]);
+				taken.set(a[p]);
 			}
 		}
 
 		for (int j = 1; j < 10; j ++) {
-			if (taken.contains(j)) continue;
+			if (taken.get(j)) continue;
 			int[] b = new int[81];
 			System.arraycopy(a, 0, b, 0, 81);
 			b[i] = j;
-			int maybeAns = solve(b);
-			if (maybeAns > 0) {
-				return maybeAns;
+			int answer = solve(b);
+			if (answer > 0) {
+				return answer;
 			}
 		}
 		return -1;
@@ -47,14 +41,15 @@ class Solution {
 			for (int n = 0; n < 50; n ++) {
 				in.readLine();
 				int[] a = new int[81];
+				int curr = 0;
 				for (int i = 0; i < 9; i ++) {
 					String s = in.readLine();
 					for (int j = 0; j < 9; j ++) {
-						a[i*9 + j] = s.charAt(j) - '0';
+						a[curr] = s.charAt(j) - '0';
+						curr ++;
 					}
 				}
-				int m = solve(a);
-				total += m;
+				total += solve(a);
 			}
 			System.out.println(total);
 		} catch (Exception e) {
