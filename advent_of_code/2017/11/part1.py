aac095c236cc61e9c97e532db59afff11c4623a1
iandioch <iandioch11@gmail.com>
2017-12-11 08:06:53 -0500

    Add advent of code day 11 part 1 in py3

---
+++ advent_of_code/2017/11/part1.py
@@ -0,0 +1,28 @@
+class CubeHex:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def dist_to_hex(self, other):
+        return self.dist_to_coord(other.x, other.y, other.z)
+
+    def dist_to_coord(self, i, j, k):
+        return (abs(self.x - i) + abs(self.y - j) + abs(self.z - k))//2
+
+    def neighbours(self):
+        return {
+            'n': CubeHex(self.x, self.y + 1, self.z - 1),
+            'ne': CubeHex(self.x + 1, self.y, self.z - 1),
+            'se': CubeHex(self.x + 1, self.y - 1, self.z),
+            's': CubeHex(self.x, self.y - 1, self.z + 1),
+            'sw': CubeHex(self.x - 1, self.y, self.z + 1),
+            'nw': CubeHex(self.x - 1, self.y + 1, self.z),
+        }
+        
+path = input().split(',')
+curr = CubeHex(0, 0, 0)
+for step in path:
+    curr = curr.neighbours()[step]
+
+print(curr.dist_to_coord(0,0,0))
