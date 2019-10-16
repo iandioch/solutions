4d4fd9ea86c04074caca2bd0c34583f553b36c91
Noah Ó Donnaile <iandioch@users.noreply.github.com>
2019-10-16 20:48:32 +0200

    Add leetcode prob sqrtx in python3

---
+++ leetcode/sqrtx/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return 1
+        if x == 0:
+            return 0
+        lo = 1
+        hi = x
+        ans = 0
+        while lo < hi:
+            mid = (lo + hi)//2
+            sq = mid*mid
+            if sq == x:
+                return mid
+            if sq < x:
+                ans = mid
+                lo = mid + 1
+            else:
+                hi = mid
+        return ans
