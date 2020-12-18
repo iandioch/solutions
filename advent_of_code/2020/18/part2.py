ebc2dbfda5d0098d1f35eba6eddf58ebde63d5de
iandioch <iandioch11@gmail.com>
2020-12-18 15:40:54 +0000

    Add Advent of Code day 18 part 2

---
+++ advent_of_code/2020/18/part2.py
@@ -0,0 +1,62 @@
+import sys
+from collections import deque
+
+def do_addition(inp):
+    pos = -1
+    for i, c in enumerate(inp):
+        if c == '+':
+            pos = i
+            break
+    if pos < 0:
+        return inp, False
+    return inp[:pos-1]  + [inp[pos-1] + inp[pos+1]] + inp[pos+2:], True
+
+def custom_eval(inp):
+    while True:
+        inp, made_step = do_addition(inp)
+        if not made_step:
+            break
+    tot = inp[0]
+    op = None
+    for c in inp[1:]:
+        if c in set(['+', '*']):
+            op = c
+        else:
+            tot = eval('{} {} {}'.format(tot, op, c))
+    return tot
+
+def remove_bracket(inp):
+    start = -1
+    for i, item in enumerate(inp):
+        if item == '(':
+            start = i
+    if start < 0:
+        return inp, False
+    tot = 0
+    end = len(inp)
+    for i in range(start, len(inp)):
+        if inp[i] == ')':
+            end = i
+            break
+    ans = custom_eval(inp[start + 1:end])
+    return inp[:start] + [ans] + inp[end+1:], True
+
+def main():
+    ans = 0
+    for line in sys.stdin.readlines():
+        items = []
+        for c in line.strip():
+            if c == ' ':
+                continue
+            if c in '+*()':
+                items.append(c)
+            else:
+                items.append(int(c))
+        while len(items) > 1:
+            items, made_step = remove_bracket(items)
+            if not made_step:
+                break
+        ans += (custom_eval(items))
+    print(ans)
+
+main()
