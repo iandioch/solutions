81feb14d5eb243f9b00a94d0710a012d1bb24b0f
iandioch <iandioch11@gmail.com>
2021-12-03 13:38:12 +0000

    Add full points solution for kattis fluortanten

--- kattis/fluortanten/solution.py
+++ kattis/fluortanten/solution.py
@@ -1,12 +1,19 @@
-def calc_pos(vals, pos):
-    tot = sum(vals[i]*(i+1) for i in range(pos)) + \
-          sum(vals[i]*(i+2) for i in range(pos, len(vals)))
-    return tot
-
 def main():
     n = int(input())
     vals = list(x for x in map(int, input().split()) if x != 0)
-    best = max(calc_pos(vals, i) for i in range(len(vals) + 1))
+
+    # This is how it would be were Bjorn at the end of the line.
+    curr = sum(vals[i]*(i+1) for i in range(len(vals)))
+    best = curr
+
+    # Shift Bjorn forwards in line one space at a time and see
+    # what the score would be then.
+    for i in range(len(vals)-1, -1, -1):
+        curr -= vals[i]*(i+1)
+        curr += vals[i]*(i+2)
+        if curr > best:
+            best = curr
+
     print(best)
 
 main()
