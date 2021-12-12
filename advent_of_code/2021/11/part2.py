62be0a4ff882b016dbf58fa7d31d09e43357c38f
iandioch <iandioch11@gmail.com>
2021-12-12 11:24:21 +0000

    Add advent of code 2021 day 11 part 2

---
+++ advent_of_code/2021/11/part2.py
@@ -0,0 +1,50 @@
+import sys
+
+NEIGHBOURS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1)
+]
+
+def main():
+    grid = [list(map(int, line.strip())) for line in sys.stdin.readlines()]
+    tot = 0
+    for step in range(1000000):
+        for row in grid:
+            for i in range(len(row)):
+                row[i] += 1
+
+        flashed = set()
+        done = False
+        while not done:
+            done = True
+            for j in range(len(grid)):
+                for i in range(len(grid[i])):
+                    if grid[j][i] > 9:
+                        flashed.add((j, i))
+                        grid[j][i] = -10000000
+                        done = False
+                        for neighbour in NEIGHBOURS:
+                            p = i + neighbour[0]
+                            q = j + neighbour[1]
+                            if (q >= 0 and q < len(grid) and p >= 0 and p < len(grid[0])):
+                                grid[q][p] += 1
+
+        for j, i in flashed:
+            grid[j][i] = 0
+
+        if len(flashed) == (len(grid)*len(grid[0])):
+            print(f'{len(flashed)} flashed on step {step+1}.')
+            return
+
+    print(tot)
+
+main()
+
+
+
