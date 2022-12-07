990097ba916ab26be89d8540545dce2fec309672
iandioch <iandioch11@gmail.com>
2022-12-07 14:54:33 +0000

    add advent of code 2022 day 6 part 1

---
+++ advent_of_code/2022/06/part1.py
@@ -0,0 +1,25 @@
+import sys
+import collections
+
+def findind(line, windowsize = 4):
+    def all_unique(d):
+        return len(set(d)) == len(d)
+
+    window = collections.deque()
+    for i in range(len(line)):
+        if len(window) < windowsize:
+            window.append(line[i])
+            continue
+
+        if all_unique(window):
+            return i
+
+        window.popleft()
+        window.append(line[i])
+
+def main():
+    for line in sys.stdin.readlines():
+        line = line.strip()
+        print(findind(line))
+
+main()
